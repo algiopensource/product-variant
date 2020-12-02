c1afef07cb76c7732ad1ad0a76a2a16ded84eac1
victorALGIOS <Algios2017>
2020-12-02 09:30:16 +0100

    remove first pull

--- product_variant_template_data/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Alex Comba <alex.comba@agilebg.com>
-
-from . import test_product_variant_template_data
--- stock_picking_variant_mgmt/wizard/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import stock_manage_variant
-from . import stock_transfer_manage_variant
